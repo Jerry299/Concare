e2f5d470718b792b00a224bfca2d29451f191b0b
Jerry299 <josephjeremiah564@yahoo.com>
2020-12-19 06:48:44 +0100

    initial commit ooo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>hello World and React </h2>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <Container>
+      <header>
+        <div>This is a div</div>
+      </header>
+    </Container>
+  );
+};
+
+export default Header;
